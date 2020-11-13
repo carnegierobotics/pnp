0dcb44e2021418897b7ad0ff6ba042a5f0f98f00
Blake Karwoski <blakekarwoski@gmail.com>
2020-11-13 15:55:34 -0500

    indentation

--- parameters.h
+++ parameters.h
@@ -31,35 +31,35 @@ namespace cvl{
 class PnpParams {
 public:
 
-    PnpParams(double threshold, double min_probability, uint max_iterations):
-              threshold(threshold), min_probability(min_probability), max_iterations(max_iterations){}
+PnpParams(double threshold, double min_probability, uint max_iterations):
+            threshold(threshold), min_probability(min_probability), max_iterations(max_iterations){}
 
-    double min_probability;
-    double threshold;
-    uint max_iterations;
+double min_probability;
+double threshold;
+uint max_iterations;
 
-    /**
-    * @brief get_iterations
-    * @param confidence probability of returning a valid result
-    * @param err_prob estimated portion of points that are outliers
-    * @param modelpoints min number of correspondances needed to construct a pose
-    * @param maxIters maximum number of iterations
-    * @return the number of iterations to run RANSAC
-    */
-    int get_iterations(double confidence, double err_prob, int modelpoints, int maxIters){
-        err_prob = std::max(err_prob, 0.);
-        err_prob = std::min(err_prob, 1.);
+/**
+* @brief get_iterations
+* @param confidence probability of returning a valid result
+* @param err_prob estimated portion of points that are outliers
+* @param modelpoints min number of correspondances needed to construct a pose
+* @param maxIters maximum number of iterations
+* @return the number of iterations to run RANSAC
+*/
+int get_iterations(double confidence, double err_prob, int modelpoints, int maxIters){
+    err_prob = std::max(err_prob, 0.);
+    err_prob = std::min(err_prob, 1.);
 
-        double num = std::max(1.0 - confidence, __DBL_MIN__);
-        double denom = 1.0 - std::pow(1.0 - err_prob, modelpoints);
-        if(denom < __DBL_MIN__){
-            return 0;
-        }
-        num = std::log(num);
-        denom = std::log(denom);
-
-        return denom >= 0 || -num >= maxIters*(-denom) ? maxIters : round(num/denom);
+    double num = std::max(1.0 - confidence, __DBL_MIN__);
+    double denom = 1.0 - std::pow(1.0 - err_prob, modelpoints);
+    if(denom < __DBL_MIN__){
+        return 0;
     }
+    num = std::log(num);
+    denom = std::log(denom);
+
+    return denom >= 0 || -num >= maxIters*(-denom) ? maxIters : round(num/denom);
+}
 
 };
 
